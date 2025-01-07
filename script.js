00dae3b969ae5f4465114f181ee2f44ac0e0ee0e
Ashitosh <ashitoshlavhate2@gmail.com>
2025-01-07 16:20:01 +0530

    This is more of JS

--- script.js
+++ script.js
@@ -1,1 +1,4 @@
 console.log("Hello GitHub..!");
+
+let name = prompt("Please enter you name :");
+console.log("You name accordin gto our data is '",namee,"' if the name is not correct press chnage it you going into settings >> mode >> name >> modify")
